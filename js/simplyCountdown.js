c4b219000468bfe3272bdf5870fb697c97cb8aec
Neo <n30dyn4m1c@gmail.com>
2018-05-30 23:11:30 +1000

    Update countdown time

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,10 +115,10 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
-                month: 6,
-                day: 28,
-                hours: 0,
+                year: 2018,
+                month: 10,
+                day: 17,
+                hours: 18,
                 minutes: 0,
                 seconds: 0,
                 words: {
